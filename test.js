4e56bfd41e61b4d330ca64f610833eb87d923a53
HDegroote <75906619+HDegroote@users.noreply.github.com>
2025-03-17 15:29:06 +0100

    Implement on, off and once

---
+++ test.js
@@ -0,0 +1,43 @@
+const process = require('process')
+const test = require('brittle')
+const rrp = require('resolve-reject-promise')
+const uncaughts = require('.')
+
+test('on-off flow', t => {
+  t.plan(5)
+
+  const handler = () => {
+    t.pass('Correctly registered unhandled rejection handler')
+    t.is(process.listenerCount('uncaughtException'), 1)
+    t.is(process.listenerCount('unhandledRejection'), 1)
+
+    console.log('offing')
+    uncaughts.off(handler)
+
+    t.is(process.listenerCount('uncaughtException'), 0, 'removed uncaughtException handler')
+    t.is(process.listenerCount('unhandledRejection'), 0, 'removed unhandledRejection handler')
+  }
+
+  uncaughts.on(handler)
+
+  const { reject } = rrp()
+  reject(new Error('I trigger an unandled rejection'))
+})
+
+test('once flow', t => {
+  t.plan(3)
+
+  const handler = () => {
+    t.pass('Correctly registered unhandled rejection handler')
+    t.is(process.listenerCount('uncaughtException'), 0, 'removed uncaughtException handler')
+    t.is(process.listenerCount('unhandledRejection'), 0, 'removed unhandledRejection handler')
+
+    console.log('offing')
+    uncaughts.off(handler)
+  }
+
+  uncaughts.once(handler)
+
+  const { reject } = rrp()
+  reject(new Error('I trigger an unandled rejection'))
+})
